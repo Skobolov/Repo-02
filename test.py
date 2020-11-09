6a2d8a6dfe4654b8fac51a61b3573f5645a5604f
Skobolov <antonskobolov@gmail.com>
2020-11-09 12:24:16 +0300

    s

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -14,33 +15,20 @@ capabilities = {
     }
 }
 
-driver = webdriver.Remote(
-    command_executor="http://localhost:4444/wd/hub",
-    desired_capabilities=capabilities)
-
-def calc(x):
-    return str(math.log(abs(12*math.sin(int(x)))))
-
-driver.get("http://suninjuly.github.io/explicit_wait2.html")
-
-WebDriverWait(driver, 12).until(
-        EC.text_to_be_present_in_element((By.ID, "price"), "100"))
-button = driver.find_element_by_id("book")
-button.click()
 
 
-x_element = driver.find_element_by_id("input_value")
-driver.execute_script("return arguments[0].scrollIntoView(true);", x_element)
-x = x_element.text
-y = calc(x)
+class TestPageSearch:
+    driver = webdriver.Remote(
+        command_executor="http://localhost:4444/wd/hub",
+        desired_capabilities=capabilities)
+    @allure.feature("Open pages")
+    @allure.story("Open pages Google.com")
+    @allure.severity("blocker")
+    def test_google_search(self):
+        self.driver.get("https://google.com")
+#        with allure.step("Делаем скриншот"):
+#            allure.attach(self.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
+        assert self.driver.title == "Google"
 
-input1 = driver.find_element_by_id("answer")
-driver.execute_script("return arguments[0].scrollIntoView(true);", input1)
-input1.send_keys(y)
-
-
-button2 = driver.find_element_by_id("solve")
-button2.click()
-
-time.sleep(5)
-driver.quit()
+    time.sleep(5)
+    driver.quit()
