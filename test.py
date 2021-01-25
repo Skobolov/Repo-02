d829b392ba1e7ace937850c0fd71923fe39d059f
Skobolov <antonskobolov@gmail.com>
2021-01-25 13:58:56 +0300

    sd

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# import time
 from selenium import webdriver
-import math
-import time
+
         
 capabilities = {
     "browserName": "chrome",
@@ -15,16 +15,13 @@ capabilities = {
     }
 }
 
-
 class TestPageSearch:
     driver = webdriver.Remote(
         command_executor="http://localhost:4444/wd/hub",
         desired_capabilities=capabilities)
 
-    def test_google_search(self):
-        self.driver.get("https://google.com")
-        assert self.driver.title == "Google"
+    driver.get("https://google.com")
+    assert driver.title == "Google"
 
-    time.sleep(5)
     driver.quit()
 
