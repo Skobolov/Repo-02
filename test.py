cb3b456bb8328867d54e28b2ce0b110e1dc6a434
Skobolov <antonskobolov@gmail.com>
2021-01-25 14:02:13 +0300

    sd

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.support import expected_conditions as EC
-# import time
+import time
 from selenium import webdriver
 
         
@@ -22,6 +22,6 @@ class TestPageSearch:
 
     driver.get("https://google.com")
     assert driver.title == "Google"
-
+    time.sleep(20)
     driver.quit()
 
