3f040de0dc57fff91c4679217f99aacc535815f0
Skobolov <antonskobolov@gmail.com>
2021-01-25 15:34:09 +0300

    otkat

--- test.py
+++ test.py
@@ -1,35 +1,15 @@
-import allure
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
-import math
+
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 import time
-        
-capabilities = {
-    "browserName": "chrome",
-    "browserVersion": "85.0",
-    "selenoid:options": {
-        "enableVNC": True,
-        "enableVideo": False
-    }
-}
+from selenium import webdriver
+
+driver = webdriver.Chrome()
 
 
+driver.get("https://google.com")
+assert driver.title == "Google"
+time.sleep(20)
+driver.quit()
 
-class TestPageSearch:
-    driver = webdriver.Remote(
-        command_executor="http://localhost:4444/wd/hub",
-        desired_capabilities=capabilities)
-    @allure.feature("Open pages")
-    @allure.story("Open pages Google.com")
-    @allure.severity("blocker")
-    def test_google_search(self):
-        self.driver.get("https://google.com")
-#        with allure.step("Делаем скриншот"):
-#            allure.attach(self.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
-        assert self.driver.title == "Google"
-
-    time.sleep(5)
-    driver.quit()
-    
