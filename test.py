e460759a792db4ac1a82cab09f01e1f918cb19c5
Skobolov <antonskobolov@gmail.com>
2021-01-25 11:59:02 +0300

    sd

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import allure
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -20,13 +20,9 @@ class TestPageSearch:
     driver = webdriver.Remote(
         command_executor="http://localhost:4444/wd/hub",
         desired_capabilities=capabilities)
-    @allure.feature("Open pages")
-    @allure.story("Open pages Google.com")
-    @allure.severity("blocker")
+
     def test_google_search(self):
         self.driver.get("https://google.com")
-#        with allure.step("Делаем скриншот"):s
-#            allure.attach(self.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
         assert self.driver.title == "Google"
 
     time.sleep(5)
