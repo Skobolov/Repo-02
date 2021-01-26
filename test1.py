e333808f15f341cfd0aded31190bcd080469c55f
Skobolov <antonskobolov@gmail.com>
2021-01-26 17:56:58 +0300

    s15

--- test1.py
+++ test1.py
@@ -2,10 +2,14 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+capabilities = {
+    "browserName": "chrome",
+    "browserVersion": "85.0",
+}
+
 driver = webdriver.Remote(
-    command_executor='http://localhost:4444/wd/hub',
-    desired_capabilities=DesiredCapabilities.CHROME
-)
+    command_executor="http://localhost:4444/wd/hub",
+    desired_capabilities=capabilities)
 
 def test_title():
     driver.get('https://www.google.com/')
