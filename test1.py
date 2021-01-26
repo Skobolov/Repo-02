ccc307402f397172aa98d4dad624e06b37bddcbb
Skobolov <antonskobolov@gmail.com>
2021-01-26 12:32:26 +0300

    sddd

---
+++ test1.py
@@ -0,0 +1,17 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+driver = webdriver.Remote(
+    command_executor='http://127.0.0.1:4444/wd/hub',
+    desired_capabilities=DesiredCapabilities
+)
+
+
+def test_title():
+    driver.get('https://www.google.com/')
+    assert "Google" in driver.title
+
+
+time.sleep(20)
+driver.quit()
