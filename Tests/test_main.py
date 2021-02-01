6f31525ce7f469e4692cc6cd0e362e5a8e0ef88e
Skobolov <antonskobolov@gmail.com>
2021-02-01 15:54:30 +0300

    Add conftest

--- Tests/test_main.py
+++ Tests/test_main.py
@@ -1,10 +1,10 @@
 link = 'https://www.google.com/'
 
-def test_title_1(driver):
-    driver.get(link)
-    assert "Google" in driver.title
+def test_title_1(browser):
+    browser.get(link)
+    assert "Google" in browser.title
 
-def test_title_2(driver):
-    driver.get(link)
-    assert "Gogle" in driver.title
+def test_title_2(browser):
+    browser.get(link)
+    assert "Gogle" in browser.title
 
