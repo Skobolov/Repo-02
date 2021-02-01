7f8025937dcda28a49aa3677d7e58bbe8cc47eda
Skobolov <antonskobolov@gmail.com>
2021-02-01 15:50:38 +0300

    Add conftest

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -2,13 +2,31 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-@pytest.fixture()
-def driver():
-    driver = webdriver.Remote(
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Remote(
+        command_executor='http://localhost:4444/wd/hub',
+        desired_capabilities=DesiredCapabilities.CHROME
+        )
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Remote(
         command_executor='http://localhost:4444/wd/hub',
         desired_capabilities=DesiredCapabilities.FIREFOX
         )
-    yield driver
-    driver.quit()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+
+
+    yield browser
+    browser.quit()
 
 
