190331639dd6a9f8fe9af2ac7a4b0754b2d1462c
Skobolov <antonskobolov@gmail.com>
2021-02-01 13:26:02 +0300

    Add conftest

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -2,13 +2,17 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=chrome,
+                     help="Choose browser: chrome or firefox")
+
 @pytest.fixture()
-def brChrome():
-    brChrome = webdriver.Remote(
+def driver():
+    driver = webdriver.Remote(
         command_executor='http://localhost:4444/wd/hub',
         desired_capabilities=DesiredCapabilities.CHROME
         )
-    yield brChrome
-    brChrome.quit()
+    yield driver
+    driver.quit()
 
 
--- Tests/test.py
+++ Tests/test.py
@@ -1,8 +1,8 @@
 link = 'https://www.google.com/'
 
-def test_title_1(brChrome):
-    brChrome.get(link)
-    assert "Google" in brChrome.title
+def test_title_1(driver):
+    driver.get(link)
+    assert "Google" in driver.title
 
 def test_title_2(brChrome):
     brChrome.get(link)
