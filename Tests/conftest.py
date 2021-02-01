79431b6ac949dcb1598db6a3a836f884d2a0c68e
Skobolov <antonskobolov@gmail.com>
2021-02-01 13:48:37 +0300

    Add conftest

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -2,10 +2,6 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=chrome,
-                     help="Choose browser: chrome or firefox")
-
 @pytest.fixture()
 def driver():
     driver = webdriver.Remote(
