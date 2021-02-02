0e82eca6ccc3b35f286f3307919ed441f6275f45
Skobolov <antonskobolov@gmail.com>
2021-02-02 12:35:59 +0300

    Add Test and use two browsers

--- Tests/test_main.py
+++ Tests/test_main.py
@@ -1,9 +1,12 @@
+import pytest
+
 link = 'https://www.google.com/'
 
 def test_title_1(browser):
     browser.get(link)
     assert "Google" in browser.title
 
+@pytest.mark.xfail
 def test_title_2(browser):
     browser.get(link)
     assert "Gogle" in browser.title
